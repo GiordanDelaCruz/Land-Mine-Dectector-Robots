cacd921bd572664becf12397011d43d002109f90
GiordanDelaCruz <GiordanDelaCruz@gmail.com>
2022-02-03 00:31:40 -0500

    Created new functions to get rover orientation, initialize path map, and update rover path map.

--- land-mine-robots.py
+++ land-mine-robots.py
@@ -45,41 +45,13 @@ class myThread (threading.Thread):
 
 
 # Declare Functions
-def single_thread(rover_num):
-    create_path_file(rover_num)
 
-
-def stask():
-    print('Starting a task in a single thread ... Thread id... ', threading.get_ident(), '...OS Thread ID...', threading.get_native_id())
-    create_path_files()
-
-def ptask(rover_num):
-    t = threading.current_thread()
-    print('Starting a multi-threaded task ... Thread name... ', t.name, '....Thread ID...', threading.get_ident(), '...OS Thread ID...', threading.get_native_id())
-   
-    api_link = base_link + str(rover_num)
-    response = requests.get(api_link)
-
-     # 1.1 Query only specific parameters from the JSON String
-    moveSeq = response.json()['data']['moves']
-    print("\nRover {roverNum} Move Data:".format(roverNum = rover_num))
-    print(moveSeq)
-
-    # 2. create file & save each respective rover data
-    dir_path = 'rover-data'
-    file_name = "path_" + str(rover_num) + ".txt"
-    file_path = os.path.join(dir_path, file_name)
-
-    # 2.1 save rover's path
-    # TODO: change text file to save path rather than move sequence 
-    file = open(file_path, "w")
-    # file.write(moveSeq)
-    file.close()
-
-    print('done\n')
-
-# single thread
+# TODO: Finish Implementation
+# Create file path
 def create_path_file(rover_num):
+
+    # initialize rover path file
+    init_rover_path(rover_num)
 
     # 1. read rover data from API
     api_link = base_link + str(rover_num)
@@ -89,18 +61,18 @@ def create_path_file(rover_num):
     # moveSeq = response.json()['data']['moves']
     moveSeq = "MMLMLMRMM"
 
-    # 2. create file & save each respective rover data
-    dir_path = 'rover-data'
-    file_name = "path_" + str(rover_num) + ".txt"
-    file_path = os.path.join(dir_path, file_name)
+    # DEBUG
+    print("\nRover {num}\n".format(num = rover_num))  
 
-    # 2.1 save rover's path
-    # TODO: change text file to save path rather than move sequence 
-    file = open(file_path, "w")
+    for action in moveSeq:
 
-    # 3. plot map
-    # TODO: Implement path of rover
-    # [straight, right, left]
+        rover_char = rover_orientation(action)
+        # update_rover_path(rover_char, path_2d_arr)
+
+# Determine rover orientation
+def rover_orientation(action):
+
+    # Initialize values
     direction = Direction.DOWN
     rover_ori = "V"
     prev_rover_ori = "V"
@@ -108,113 +80,64 @@ def create_path_file(rover_num):
     clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
     idx = clock_wise.index(direction)
 
-    # Read map 
-    map_2d_arr = read_map_data("map.txt")
-    path_2d_arr = map_2d_arr
+    # 3.1 move forward (M)
+    if action == "M":
+        new_dir = clock_wise[idx] # no change  NOTE: POTENTIALLY DO NOT NEED THIS
 
+    # 3.2 move left (L)
+    elif action == "L":
+        next_idx = (idx + 1) % 4
+        new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        
+    # 3.3 move right(R)    
+    elif action == "R":
+        next_idx = (idx - 1) % 4
+        new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+    
+    # 3.4 dig mine (D)
+    else: 
+        pass
+        # next_idx = (idx - 1) % 4
+        # new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
+    direction = new_dir
+
+    # Determine rover orientation
+    # Case #1: Rover faces SOUTH
+    if prev_rover_ori == "V": 
+        if direction.name == "LEFT":
+            rover_ori = ">"
+        elif direction.name == "RIGHT":
+            rover_ori = "<"
+
+    # Case 2: Rover faces EAST
+    elif prev_rover_ori == ">":
+        if direction.name == "LEFT":
+            rover_ori = "^"
+        elif direction.name == "RIGHT":
+            rover_ori = "V"
+
+    # Case 3: Rover faces NORTH
+    elif prev_rover_ori == "^":
+        if direction.name == "LEFT":
+            rover_ori = "<"
+        elif direction.name == "RIGHT":
+            rover_ori = ">"
+            
+    # Case 4: Rover faces WEST
+    elif prev_rover_ori == "<":
+        if direction.name == "LEFT":
+            rover_ori = "V"
+        elif direction.name == "RIGHT":
+            rover_ori = "^"
+
+    # Save previous orientation
+    prev_rover_ori = rover_ori
 
     # DEBUG
-    print("\nRover {num}\n".format(num = rover_num))  
+    print("Move: {move}. Direction: {name}, Value: {value}. Rover Orientation: {rover_ori}".format( move = action, name = direction.name, value = direction.value, rover_ori = rover_ori) )
 
-    # TODO: Fix orientation movement (e.g right & left turns)
-    for char in moveSeq:
-        
-        # 3.1 move forward (M)
-        if char == "M":
-            new_dir = clock_wise[idx] # no change
-
-        # 3.2 move left (L)
-        elif char == "L":
-            next_idx = (idx + 1) % 4
-            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
-            
-        # 3.3 move right(R)    
-        elif char == "R":
-            next_idx = (idx - 1) % 4
-            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
-        
-        # 3.4 dig mine (D)
-        else: 
-            pass
-            # next_idx = (idx - 1) % 4
-            # new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
-
-        direction = new_dir
-
-        # Determine rover orientation
-        # TODO: Determine correct way to determine rover orientation
-        # Case #1: Rover faces SOUTH
-        if prev_rover_ori == "V":
-            if direction.name == "LEFT":
-                rover_ori = ">"
-            elif direction.name == "RIGHT":
-                rover_ori = "<"
-
-        # Case 2: Rover faces EAST
-        elif prev_rover_ori == ">":
-            if direction.name == "LEFT":
-                rover_ori = "^"
-            elif direction.name == "RIGHT":
-                rover_ori = "V"
-
-        # Case 3: Rover faces NORTH
-        elif prev_rover_ori == "^":
-            if direction.name == "LEFT":
-                rover_ori = "<"
-            elif direction.name == "RIGHT":
-                rover_ori = ">"
-
-        # Case 4: Rover faces WEST
-        elif prev_rover_ori == "<":
-            if direction.name == "LEFT":
-                rover_ori = "V"
-            elif direction.name == "RIGHT":
-                rover_ori = "^"
-
-        # Save previous orientation
-        prev_rover_ori = rover_ori
-        
-        print("Direction: {name}, Value: {value}. Rover Orientation: {rover_ori}".format( name = direction.name, value = direction.value, rover_ori = rover_ori) )
-
-
-# multithread
-def create_path_files():
-
-    # 1. read rover data from API
-    for i in range(1, 11):
-        api_link = base_link + str(i)
-        response = requests.get(api_link)
-
-        # 1.1 Query only specific parameters from the JSON String
-        moveSeq = response.json()['data']['moves']
-        # print("\nRover {roverNum} Move Data:".format(roverNum = i))
-        # print(moveSeq)
-
-        # 2. create file & save each respective rover data
-        dir_path = 'rover-data'
-        file_name = "path_" + str(i) + ".txt"
-        file_path = os.path.join(dir_path, file_name)
-
-        # 2.1 save rover's path
-        # TODO: change text file to save path rather than move sequence 
-        file = open(file_path, "w")
-
-        # 3. plot map
-        # TODO: Implement path of rover
-
-        direction = down
-        for char in moveSeq:
-            pass
-
-            # 3.1 move forward (M)
-            
-            # 3.2 move left (L)
-         
-            # 3.3 move right(R)
-           
-            # 3.4 dig mine (D)
-
-# 4. read map data
+# Read map data
 def read_map_data(text_file):
     with open(text_file) as file:
         firstline = file.readline()
@@ -244,16 +167,57 @@ def read_map_data(text_file):
         rowIdx += 1
         colIdx = 0
 
-    return map_2d_arr
     # DEBUG
     # print("map in 2D:\n", map_2d_arr)
 
+    return map_2d_arr
+  
+# Initialize rover path file
+def init_rover_path(rover_num):
 
-# Read map
-map_2d_arr = read_map_data("map.txt")
-print("map in 2D:\n", map_2d_arr)
+     # create file & save each respective rover data
+    dir_path = 'rover-data'
+    file_name = "path_" + str(rover_num) + ".txt"
+    file_path = os.path.join(dir_path, file_name)
+
+    # save rover's path
+    # TODO: change text file to save path rather than move sequence 
+    file = open(file_path, "w")
+
+    # read map 
+    map_2d_arr = read_map_data("map.txt")
+    path_2d_arr = map_2d_arr
+
+    num_of_row = len(path_2d_arr)
+    num_of_col = len(path_2d_arr[0])
+    counter = 0
+
+    # DEBUG
+    print("num of row: {row}, num of col: {col}".format(row = num_of_row, col = num_of_col))
+
+    # load map data into rover path file 
+    for row in path_2d_arr:
+        for char in row:
+            
+            if counter < 5:
+                file.write(char)
+                counter = counter + 1
+            else:  
+                string = "\n" + char
+                file.write(string)
+                counter = 1
+
+# TODO: Implement        
+# Update rover path map  
+def update_rover_path(rover_char, path_2d_arr):
+    pass
+
+    
 
 
+# ----------------------------------------------------------------------------------------
+#                                       Main body
+# ----------------------------------------------------------------------------------------
 threadLock = threading.Lock()
 threads = []
 
@@ -278,58 +242,8 @@ print("Exiting Main Thread")
 
 
 # Part 1: Sequential Program
-single_thread(10)
+create_path_file(1)
 
 # Part 2: Parallel Program
-
-
-
-# run single thread & time it
-# start_time = perf_counter()
-# stask()
-# end_time = perf_counter()
-# print(f'\nIt took {end_time- start_time: 0.2f} second(s) to complete.\n\n')
-
-
-# # run multithread & time it
-# start_time = perf_counter()
-
-# # create new threads
-# t1 = Thread( target=ptask(1) )
-# t2 = Thread( target=ptask(2) )
-# t3 = Thread( target=ptask(3) )
-# t4 = Thread( target=ptask(4) )
-# t5 = Thread( target=ptask(5) )
-# t6 = Thread( target=ptask(6) )
-# t7 = Thread( target=ptask(7) )
-# t8 = Thread( target=ptask(8) )
-# t9 = Thread( target=ptask(9) )
-# t10 = Thread( target=ptask(10) )
-
-# # start the threads
-# t1.start()
-# t2.start()
-# t3.start()
-# t4.start()
-# t5.start()
-# t6.start()
-# t7.start()
-# t8.start()
-# t9.start()
-# t10.start()
-
-# # wait for the threads to complete
-# t1.join()
-# t2.join()
-# t3.join()
-# t4.join()
-# t5.join()
-# t6.join()
-# t7.join()
-# t8.join()
-# t9.join()
-# t10.join()
-
-# end_time = perf_counter()
-
-# print(f'It took {end_time- start_time: 0.2f} second(s) to complete.')
+# for i in range(1, 11):
+#     create_path_file(i)
